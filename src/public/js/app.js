fed5ee778038edb6d73bbb6184ca1ad1e995a29c
euiyang <euiyang2000@gmail.com>
2023-03-04 12:11:03 +0900

    3. stream with socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,91 +1,95 @@
 const socket=io();
 
-const welcome=document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room=document.getElementById("room");
-
-room.hidden=true;
-
-let roomName;
+const myFace=document.getElementById("myFace");
+const muteBtn=document.getElementById("mute");
+const cameraBtn=document.getElementById("camera");
+const camerasSelect=document.getElementById("cameras");
 
 
+let myStream;
+//stream= audio+ video
+let muted=false;
+let cameraOff=false;
 
-function addMessage(message){
-    const ul=room.querySelector("ul");
-    const li=document.createElement("li");
-    li.innerText=message;
-    ul.appendChild(li);
-}
+async function getCameras(){
+    try{
+        const devices=await navigator.mediaDevices.enumerateDevices();
+        //장치 전부 검색
+        const cameras=devices.filter(divied=>devices.kind==="videoinput");
+        //videoinput에 해당하는 장치 필터링
+        const currentCamera=myStream.getVideoTracks()[0];
+        cameras.forEach(camera=>{
+            const option=document.createElement("option");
+            option.value=camera.deviceId;
+            option.innerText=camera.label;
 
-function backendDone(msg){
-    console.log("backend says "+msg);
-}
-
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input=room.querySelector("#msg input");
-    const value=input.value;
-    socket.emit("new_message",input.value,roomName,()=>{
-        addMessage(`you: ${value}`);
-    });
-    input.value="";
-}
-
-function handleNicknameSubmit(event){
-    event.preventDefault();
-    const input=room.querySelector("#name input");
-    const value=input.value;
-    socket.emit("nickname",input.value);
-}
-
-function showRoom(){
-    welcome.hidden=true;
-    room.hidden=false;
-    const h3=room.querySelector("h3");
-    h3.innerText=`Room ${roomName}`;
-    const msgForm=room.querySelector("#msg");
-    const nameForm=room.querySelector("#name")
-    msgForm.addEventListener("submit",handleMessageSubmit);
-    nameForm.addEventListener("submit",handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event){
-    event.preventDefault();
-    const input=form.querySelector("input");
-    socket.emit("enter_room",input.value,showRoom);
-    //이전 websocket과 달리 어떠한 이벤트든 반응 가능하고 string으로 보낼 필요 없음
-    //emit 함수의 마지막 인자로 들어가는 callback은 서버 측에서 실행되는 함수를 의미함
-    roomName=input.value;
-    input.value="";
-}
-
-form.addEventListener("submit",handleRoomSubmit);
-
-socket.on("welcome",(user,newCount)=>{
-    const h3=room.querySelector("h3");
-    h3.innerText=`Room ${roomName} (${newCount})`;
-    addMessage(`${user} arrived`);
-});
-
-socket.on("bye",(left,newCount)=>{
-    const h3=room.querySelector("h3");
-    h3.innerText=`Room ${roomName} (${newCount})`;
-    addMessage(`${left} left`);
-})
-
-socket.on("new_message",addMessage);
-
-socket.on("room_change",console.log);
-
-socket.on("room_change",(rooms)=>{
-    roomList.innerHTML="";
-    if(rooms.length===0){
-        return;
+            if(currentCamera.label==camera.label){
+                option.selected=true;
+            }// 현재 사용중인 장치 확인 코드
+            
+            camerasSelect.appendChild(option);
+        })
+        //장치 선택하도록 설정
+    }catch(e){
+        console.log(e);
     }
-    const roomList=welcome.querySelector("ul");
-    rooms.forEach((room)=>{
-        const li=document.createElement("li");
-        li.innerText=room;
-        roomList.append(li);
-    })
-})
+}
+
+async function getMedia(deviceId){
+    const initialConstrains={
+        audio:true,
+        video:{facingMode:"user"},
+    };
+
+    const cameraConstrains={
+        audio:true,
+        video:{deviceId:{exact: deviceId}},
+    };
+
+    try{
+        myStream=await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstrains : initialConstrains
+        );
+        myFace.srcObject=myStream;
+        if(!deviceId){
+            await getCameras();
+        }
+        
+    }catch(e){
+        console.log(e);
+    }
+}
+
+getMedia();
+
+function handleMuteClick(){
+    myStream.getAudioTracks().forEach((track)=>(track.enabled=!track.enabled));
+    //트랙에 접근 가능 enable<->disable 수정 코드
+    if(!muted){
+        muteBtn.innerText="Unmute";
+        muted=true;
+    }else{
+        muteBtn.innerText="Mute";
+        muted=false;
+    }
+}
+
+function handleCameraClick(){
+    myStream.getVideoTracks().forEach((track)=>(track.enabled=!track.enabled));
+    if(cameraOff){
+        cameraBtn.innerText="Turn Camera On";
+        cameraOff=false;
+    }else{
+        cameraBtn.innerText="Turn Camera Off";
+        cameraOff=true;
+    }
+}
+
+function handleCameraChange(){
+    getMedia(camerasSelect.value);
+}
+//장치 변경시 id를 변경하는 것이 중요
+
+muteBtn.addEventListener("click",handleMuteClick);
+cameraBtn.addEventListener("click",handleCameraClick);
+camerasSelect.addEventListener("input",handleCameraChange);
